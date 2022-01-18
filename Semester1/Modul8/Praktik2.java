c74fc022f22299e29c33e788def644f9d3c0448c
“bagussub18” <bagussub18@gmail.com>
2022-01-18 11:08:44 +0700

    Praktik 2

---
+++ Semester1/Modul8/Praktik2.java
@@ -0,0 +1,11 @@
+package Semester1.Modul8;
+
+public class Praktik2 {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a <= 10) {
+            System.out.println(a);
+            a+=2;
+        }
+    }
+}
